98dc2967cd6c3b6283a64d2a0516d038067f3db7
Kamil <krikus@gmail.com>
2024-04-13 12:45:56 +0200

    feat: lsp and harper setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,9 @@
 local lsp_zero = require('lsp-zero')
+local lspconfig = require('lspconfig')
 
-lsp_zero.on_attach(function(client, bufnr)
+---@diagnostic disable-next-line: unused-local
+local on_attach = function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -12,57 +13,26 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+end
 
--- to learn how to use mason.nvim with lsp-zero
--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'tsserver', -- Js and Ts
-    'rust_analyzer', -- Rust
-    'eslint', -- ESlint
-    'intelephense', -- PHP language server.
-    'docker_compose_language_service', -- Docker Compose Language Server.
-  },
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-      require('lspconfig').harper_ls.setup {}
-    end,
-  }
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
--- this is the function that loads the extra snippets to luasnip
--- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format({details = false}),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-})
+lsp_zero.on_attach = on_attach
 
 -- https://github.com/MannyFay/nvim/blob/main/nvim/lua/user/plugin_options/lspconfig.lua#L48
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig').intelephense.setup({
+
+require('mason-tool-installer').setup({
+  ensure_installed = {
+    -- 'eslint_d',
+    'harper-ls'
+  }
+})
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+
+
+
+lspconfig.intelephense.setup({
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
     -- client.server_capabilities.documentFormattingProvider      = false
@@ -72,7 +42,7 @@ require('lspconfig').intelephense.setup({
   environment = {phpVersion = '8.0.0'},
 })
 
-require('lspconfig').rust_analyzer.setup({
+lspconfig.rust_analyzer.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
@@ -99,4 +69,93 @@ require('lspconfig').rust_analyzer.setup({
       },
     }
   }
+})
+
+local config_path = vim.fn.stdpath('config')
+
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MasonToolsUpdateCompleted',
+  callback = function(e)
+    vim.schedule(function()
+      lspconfig.harper_ls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          ["harper-ls"] = {
+            userDictPath = config_path .. '/harper-dicts/user.txt',
+            linters = {
+              spell_check = true,
+              spelled_numbers = false,
+              an_a = true,
+              sentence_capitalization = false,
+              unclosed_quotes = true,
+              wrong_quotes = false,
+              long_sentences = true,
+              repeated_words = true,
+              spaces = true,
+              matcher = true
+            }
+          }
+        }
+      }
+      print(vim.inspect(e.data)) -- print the table that lists the programs that were installed
+    end)
+  end,
+})
+
+
+--- lets try putting this as last :)
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'tsserver', -- JS and TS
+    'eslint',
+    'rust_analyzer', -- Rust
+    'intelephense', -- PHP language server.
+    'docker_compose_language_service', -- Docker Compose Language Server.
+    'lua_ls',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      lspconfig.lua_ls.setup(lua_opts)
+    end,
+    eslint = function()
+      lspconfig.eslint.setup({
+        settings = {
+          workingDirectory = { mode = 'location' },
+        },
+        root_dir = require('lspconfig/util').root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git', 'eslintrc.js', 'eslint.js', 'node_modules'),
+      })
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {
+  behavior = cmp.SelectBehavior.Select
+}
+
+
+-- This is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  formatting = lsp_zero.cmp_format({details = false}),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
--- lua/krikus/packer.lua
+++ lua/krikus/packer.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'WhoIsSethDaniel/mason-tool-installer.nvim'
   use {
     'm4xshen/smartcolumn.nvim',
     config = function()
